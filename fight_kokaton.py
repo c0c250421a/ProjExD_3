be65698801078544c3b8c9e6f8d7d5921d48661b
tajimaf <c0c250421a@edu.teu.ac.jp>
2026-06-23 14:39:06 +0900

    練習3 喜ぶエフェクト追加-3

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -163,8 +163,6 @@ def main():
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
                 pg.display.update()
-                time.sleep(1)
-                return
         
         if bomb is not None:
             if beam is not None:
