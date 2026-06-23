dc37b32b441b7e01b0c4b3bd1d91cdca14097460
tajimaf <c0c250421a@edu.teu.ac.jp>
2026-06-23 14:36:47 +0900

    練習3 喜ぶエフェクト追加-2

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -168,7 +168,7 @@ def main():
         
         if bomb is not None:
             if beam is not None:
-                if beam.rct.colliderect(bomb.rct):
+                if beam.rct.colliderect(bomb.rct):  # ビームで爆弾を打ち落としたら
                     bird.change_img(6, screen)
                     beam = None
                     bomb = None
